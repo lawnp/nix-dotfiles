ab26b87cf915e2106450fab382c8c70d166e1fdb
Lan Pavletič <pavletic.lan@gmail.com>
2025-10-12 21:10:51 +0200

    chore: regenerate hardware configuration

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,23 +8,21 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5d902b2f-b15d-4c8a-a688-47bbb81c8642";
+    { device = "/dev/disk/by-uuid/08753712-d6e4-4da7-87ed-a4b23da4591e";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0466-E0BE";
+    { device = "/dev/disk/by-uuid/152D-54E7";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
-
-  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
