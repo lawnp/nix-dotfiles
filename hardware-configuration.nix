0f6333280acdb5329c99b74ec214bc5605f6c73e
Lan Pavletič <pavletic.lan@gmail.com>
2026-09-18 08:46:29 +0200

    chore: restore hardware-configuration.nix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,43 +8,28 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fc020a27-dc41-4205-90db-495e51759e3f";
+    { device = "/dev/disk/by-uuid/08753712-d6e4-4da7-87ed-a4b23da4591e";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/60A6-C50F";
+    { device = "/dev/disk/by-uuid/152D-54E7";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/9a77b736-d19e-4475-ba64-eb5e3605614b"; }
-    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-04789046c794.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-37fb0da17046.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-7ef3cc7563aa.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-9dd4436799cf.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-e4de6b1bfeb0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.br-fc6472c78d67.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.docker_gwbridge.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.vboxnet0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.veth2a4f8c2.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
